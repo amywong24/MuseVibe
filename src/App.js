1401710886d4f5d399881533d4ee0f7b24628074
Amy Wong <akwong24@outlook.com>
2024-06-16 00:48:44 -0700

    Added music player

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import Player from './components/Player';
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
+    <>
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar />
       </div>
+      <Player />
     </div>
+    </>
   );
 }
 
