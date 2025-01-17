62e704a3857b7a57401976192d51d488d27d94ce
Amy Wong <akwong24@outlook.com>
2025-01-16 16:27:56 -0800

    finished project!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
         <>
             <div className="w-full flex justify-between items-center font-semibold">
                 <div className="flex items-center gap-2">
-                    <img onClick={() => navigate(-1)} className="w-8 bg-[#3C096C] p-2 rounded-2xl cursor-pointer" src={assets.arrow_left}/>
-                    <img onClick={() => navigate(1)} className="w-8 bg-[#3C096C] p-2 rounded-2xl cursor-pointer" src={assets.arrow_right}/>
+                    <img onClick={() => navigate(-1)} className="w-8 bg-[#3C096C] p-2 rounded-2xl cursor-pointer" src={assets.arrow_left} alt="left arrow"/>
+                    <img onClick={() => navigate(1)} className="w-8 bg-[#3C096C] p-2 rounded-2xl cursor-pointer" src={assets.arrow_right} alt="right arrow"/>
                 </div>
                 <div className="flex items-center gap-4">
                     <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer">Explore Premium</p>
